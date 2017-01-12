d445fd4eb07de762a652873cbfb60129400ea843
Carlota Bindner <cev.bindner@gmail.com>
2017-01-12 19:59:33 +0000

    Adding solutions to variables exercises

---
+++ variable_solutions/name.rb
@@ -0,0 +1,17 @@
+# name.rb
+
+puts "What is your name?"
+name = gets.chomp
+puts "Hi #{name}, nice to meet you."
+
+# name.rb solution to exercise 3
+10.times do
+    puts name
+end
+
+# name.rb solution to exercise 4
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "#{first_name} #{last_name}"
